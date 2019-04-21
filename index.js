acf18ec6eb08ecd0a37bf2f3e0fc3b864eb1b5f8
lsoft <lsoft.riverside@gmail.com>
2019-04-21 14:31:39 +0300

    Это наш первый коммит

---
+++ index.js
@@ -0,0 +1,52 @@
+
+'use strict';
+
+var leftBorderWidth = 30;
+
+const pi = 3.14;
+// {
+//     let num = 0;
+//     let var1 = 1000 / num;
+//     console.log(var1);
+// }
+
+//console.log('121' * '100' / '132');
+
+// log('hello');
+// log(1000 * pi);
+
+function log(value) {
+    {
+        console.log(value);
+    }
+}
+
+// let something = 3;
+// log(something);
+
+let person = {
+    name: 'John', 
+    age: 25, 
+    isMarried: true
+};
+
+//log(person.age + ' is age of ' + person.name);
+
+let arr = ['1.png', '2.png', 3];
+ 
+// arr.forEach(element => {
+//    log(element);
+// });
+
+// let ok = confirm('Is OK?');
+
+// if (ok) {
+//     alert('Ok');
+// }
+
+//log(1+1+'we'+1+{name: 'Vas'}.name);
+
+//log(Number.NEGATIVE_INFINITY);
+//log('\u03c0='+pi);
+let n = 128;
+log(n.toString(16));
